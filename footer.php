ddf790204970814b97d4159305246fda0f00f326
fmedina-dev <68657233+fmedina-dev@users.noreply.github.com>
2020-09-17 21:07:34 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+ <footer>
+	<div class="footer-000">
+		<div class="footer-001">
+			
+			<p class="fm"> Sobre nosotros</p> 
+			<p class="nosotros00">Desarrollado por: 
+				Roberth Loor,
+				David Zambrano,
+				Adriana Gilces y
+				Pablo Proaño.
+			</p>
+		</div>
+				
+		<div class="footer-002">
+			<p class="fm">Síguenos</p>
+            <ul class="footer-links">
+              	<li><a class="9i" href="">→ Facebook</a></li>
+             	<li><a href="">→ Twitter</a></li>
+              	<li><a href="">→ Instagram</a></li>
+            </ul>
+		</div>			
+		
+	</div>
+</footer>
+
+    
